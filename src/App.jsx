82d24a11a2fce6688e904d2f25c8bce6403af272
holivera159 <holivera0730@gmail.com>
2024-08-06 20:58:53 -0300

    Tooltip

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useCatImage } from "./Hooks/useCatImage.jsx";
 import { useCatFact } from "./Hooks/useCatFact.jsx";
 import "./index.css";
 import { useState, useEffect } from "react";
-import { Button, Spinner, Datepicker } from "flowbite-react";
+import { Button, Spinner, Datepicker, Tooltip } from "flowbite-react";
 
 export function App() {
   const { fact, refreshFact } = useCatFact();
@@ -27,14 +27,16 @@ export function App() {
 
   return (
     <main className="m-5 font-[Montserrat] p-3 rounded-2xl shadow-2xl place-content-center w-[340px] lg:w-[500px] bg-white">
-      <Datepicker className="absolute top-6 left-6 w-[170px]"/>
-      <div className="flex flex-col place-content-center items-center mb-3">
-        <h1 className="bg-slate-500 text-white text-xl flex w-[100%] place-content-center py-2 px-4 rounded-2xl mr-2">
+      <Datepicker className="absolute top-6 left-6 w-[170px]" />
+      <div className="flex flex-col place-content-center items-center mb-3 relative">
+        <h1 className="bg-slate-500 text-white text-xl flex w-[100%] place-content-center py-2 px-4 rounded-2xl mr-2 mb-4">
           App de gatitos
         </h1>
-        <Button color="dark" onClick={handleClick} className="px-5 py-1 mt-3">
-          Get new fact!
-        </Button>
+        <Tooltip content="Warning! Cute Cats..." className="">
+          <Button color="dark" onClick={handleClick} className="">
+            Get new fact!
+          </Button>
+        </Tooltip>
       </div>
       {loading ? (
         <div className="flex justify-center my-20">
